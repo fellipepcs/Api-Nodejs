acf5d89a6ad7b5063cd624b54180361cabe28f0c
fellipepcs <fellipy.02@hotmail.com>
2022-07-20 12:40:00 -0300

    Adicionado metedo de verificar variaveis da requisição

--- src/controllers/codigo_acesso.controller.ts
+++ src/controllers/codigo_acesso.controller.ts
@@ -3,21 +3,27 @@ import CodigoAcessoModel from "../models/codigo_acesso.models"
 
 
 class CodigoAcessoController {
-    public async cadastrarCodigoAcesso(req: Request, res: Response): Promise<Response> {
-        try {
-            const codigo_acesso = await CodigoAcessoModel.create(req.body);
-            const resposta = {
-                message: 'Codigo Acesso Cadastrado',
-                id: codigo_acesso._id
-            }
-            return res.json(resposta);
-        } catch (e) {
-            const message = {
-                erro: 'Não foi possivel cadastrar um codigo de acesso'
-            }
-            return res.json(message);
-        }
+  public async cadastrarCodigoAcesso(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id_imobiliario } = req.body;
+
+      if (id_imobiliario < 0) {
+        return res.json("ERRO")
+      }
+
+      const codigo_acesso = await CodigoAcessoModel.create(req.body);
+      const resposta = {
+        message: 'Codigo Acesso Cadastrado',
+        id: codigo_acesso._id
+      }
+      return res.json(resposta);
+    } catch (e) {
+      const message = {
+        erro: 'Não foi possivel cadastrar um codigo de acesso'
+      }
+      return res.json(message);
     }
+  }
 }
 
 export default new CodigoAcessoController();
--- src/controllers/contato.controller.ts
+++ src/controllers/contato.controller.ts
@@ -2,16 +2,16 @@ import ContatoSchema from "../models/contato.models"
 import { Request, Response } from "express";
 
 
-class ContatoController{
-    public async cadastrarContato(req: Request, res: Response): Promise<Response>{
-        try{
+class ContatoController {
+    public async cadastrarContato(req: Request, res: Response): Promise<Response> {
+        try {
             const contato = await ContatoSchema.create(req.body);
             const resposta = {
                 message: 'Contato Criado',
                 nome: contato.nome
             }
             return res.json(resposta)
-        } catch{
+        } catch {
             const message = {
                 erro: 'Não foi possivel cadastrar um contato'
             }
